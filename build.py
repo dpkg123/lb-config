782718544aa494e65acff882034d07a82bf7273c
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2023-11-22 14:31:38 +0800

    Update build.py

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import sys
 import os
 
 os.system("apt-get -y install git live-build debian-archive-keyring gpg dosfstools genisoimage squashfs-tools xorriso grub-common grub-pc-bin grub-efi-amd64-bin nano")
-os.system("git clone https://github.com/dpkg123/lb-config/ ci")
+os.system("git clone https://github.com/dpkg123/lb-config/ ci --depth=1")
 os.system("cd ci && lb config && lb build")
