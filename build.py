c874b264e26bf41876f006e4f5c8314747b18842
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2024-05-26 12:59:47 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,34 +1,18 @@
 import subprocess
 
-
 def run_command(command):
-    stderr=subprocess.STDOUT
-    process = subprocess.Popen(command,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               shell=True,
-                               universal_newlines=True)
-    while process.poll() is None:
-        line = process.stdout.readline().strip()
-        if line:
-            print(line)
-
-    return process.returncode
-
-def clone_repo():
-    command = "git clone https://github.com/dpkg123/lb-config/ ci --depth=1"
-    run_command(command)
-
-
-def build_repo():
-    command = "cd ci && lb config && lb build"
-    run_command(command)
-
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip().decode())
+    return process.poll()
 
 def main():
-    clone_repo()
-    build_repo()
-
+    run_command('git clone https://github.com/dpkg123/lb-config/ ci --depth=1')
+    run_command('cd ci && lb config && lb build')
 
 if __name__ == '__main__':
     main()
