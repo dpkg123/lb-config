7a7eba2c76312466f173addca92f142e2da163a1
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-05-26 12:54:56 +0800

    update packages

--- build.py
+++ build.py
@@ -1,5 +1,34 @@
-import sys
-import os
+import subprocess
 
-os.system("git clone https://github.com/dpkg123/lb-config/ ci --depth=1")
-os.system("cd ci && lb config && lb build")
+
+def run_command(command):
+    stderr=subprocess.STDOUT
+    process = subprocess.Popen(command,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True,
+                               universal_newlines=True)
+    while process.poll() is None:
+        line = process.stdout.readline().strip()
+        if line:
+            print(line)
+
+    return process.returncode
+
+def clone_repo():
+    command = "git clone https://github.com/dpkg123/lb-config/ ci --depth=1"
+    run_command(command)
+
+
+def build_repo():
+    command = "cd ci && lb config && lb build"
+    run_command(command)
+
+
+def main():
+    clone_repo()
+    build_repo()
+
+
+if __name__ == '__main__':
+    main()
